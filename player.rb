13a88fab67b6ff3f6dac90923168cae3dcb4bea1
Marcos Sandrini <msandrinidesign@gmail.com>
2022-08-24 14:21:29 +0200

    initial logic for determined hands

--- player.rb
+++ player.rb
@@ -4,10 +4,11 @@ require_relative "make_play"
 require_relative "card"
 class Player
 
-  VERSION = "v0.2.1"
+  VERSION = "v0.3.1"
 
   def bet_request(game_state)
-    current_player = game_state["players"][game_state["in_action"]]
+
+		current_player = game_state["players"][game_state["in_action"]]
     current_hole_cards = current_player["hole_cards"]
     community_cards = game_state["community_cards"]
 
@@ -20,34 +21,52 @@ class Player
       current_player["stack"]
     end
 
-    if current_hole_cards.any?{ |c| (%w[J Q K A 10]).include?(c["rank"]) }
-      ::MakePlay.new(
-        action: "raise",
-        current_funds: current_player["stack"],
-        current_buy_in: game_state["current_buy_in"],
-        current_bet: current_player["bet"],
-        raise_amount: raise_amount
-      ).call
-    elsif current_hole_cards.none?{ |c| (%w[J Q K A 10]).include?(c["rank"]) }
-      ::MakePlay.new(
-        action: "fold",
-        current_funds: current_player["stack"],
-        current_buy_in: game_state["current_buy_in"],
-        current_bet: current_player["bet"],
-        raise_amount: raise_amount
-      ).call
-    else
-      ::MakePlay.new(
-        action: "call",
-        current_funds: current_player["stack"],
-        current_buy_in: game_state["current_buy_in"],
-        current_bet: current_player["bet"],
-        raise_amount: raise_amount
-      ).call
-    end
-  end
+		bet_increment = 2 * game_state["minimum_raise"]
+
+		if current_combination.count < 7
+			::MakePlay.new(
+				action: "call",
+				current_funds: current_player["stack"],
+				current_buy_in: game_state["current_buy_in"],
+				current_bet: current_player["bet"]
+			).call
+		else
+			if hand_set.pair
+				::MakePlay.new(
+					action: "raise",
+					current_funds: current_player["stack"],
+					current_buy_in: game_state["current_buy_in"],
+					current_bet: current_player["bet"],
+					raise_amount: raise_amount
+				).call
+			elsif hand_set.two_pairs || hand_set.three_of_a_kind || hand_set.flush
+				::MakePlay.new(
+					action: "raise",
+					current_funds: current_player["stack"],
+					current_buy_in: game_state["current_buy_in"],
+					current_bet: current_player["bet"],
+					raise_amount: raise_amount + bet_increment
+				).call
+			elsif hand_set.four_of_a_kind
+				::MakePlay.new(
+					action: "raise",
+					current_funds: current_player["stack"],
+					current_buy_in: game_state["current_buy_in"],
+					current_bet: current_player["bet"],
+					raise_amount: raise_amount + (bet_increment * 2)
+				).call
+			else
+				::MakePlay.new(
+					action: "fold",
+					current_funds: current_player["stack"],
+					current_buy_in: game_state["current_buy_in"],
+					current_bet: current_player["bet"],
+					raise_amount: raise_amount
+				).call
+			end
+		end
+	end
 
   def showdown(game_state)
-
   end
 end
