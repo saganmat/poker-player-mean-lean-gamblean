e2c22a6b6566a648f9007afcaf4be9b597b37474
Mateusz Sagan <mateusz@viaeurope.com>
2022-08-24 11:29:21 +0200

    Fix player identification

--- player.rb
+++ player.rb
@@ -4,12 +4,12 @@ class Player
   VERSION = "v0.1"
 
   def bet_request(game_state)
-    # current_player = game_state["players"].filter { |player| player["name"].eql?("Mean Lean Gamblean") }
-    # current_hole_cards = current_player["hole_cards"]
-    # community_cards = game_state["community_cards"]
-    #
-    # current_combination = current_hole_cards + community_cards
-    # hand_set = DetermineHandSet.new(current_combination).call
+    current_player = game_state["players"].filter { |player| player["name"].eql?("Mean Lean Gamblean") }.first
+    current_hole_cards = current_player["hole_cards"]
+    community_cards = game_state["community_cards"]
+
+    current_combination = current_hole_cards + community_cards
+    hand_set = DetermineHandSet.new(current_combination).call
 
     0
   end
