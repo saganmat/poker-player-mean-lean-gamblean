89aae25a6ab902b83470efae08b77d33ba5a33ba
Mateusz Sagan <mateusz@viaeurope.com>
2022-08-24 12:29:27 +0200

    Fix (hopefully) app crash

---
+++ make_play.rb
@@ -0,0 +1,22 @@
+class MakePlay
+
+  def initialize(action:, current_funds:, current_buy_in:, current_bet:, raise_amount:)
+    @action = action
+    @current_funds = current_funds
+    @current_buy_in = current_buy_in
+    @current_bet = current_bet
+    @raise_amount = raise_amount
+  end
+  def call
+    case action
+    when "fold"
+      0
+    when "call"
+      @current_buy_in - @current_bet
+    when "raise"
+      @current_buy_in - @current_bet + @raise_amount
+    else
+      0
+    end
+  end
+end
--- player.rb
+++ player.rb
@@ -12,20 +12,20 @@ class Player
 
     current_combination = current_hole_cards + community_cards
     hand_set = ::DetermineHandSet.new(current_combination).call
-    
+
     raise_amount = if current_player["stack"] >= game_state["minimum_raise"]
       game_state["minimum_raise"]
     else
       current_player["stack"]
     end
 
-    MakePlay.call(
+    ::MakePlay.new(
       action: "call",
       current_funds: current_player["stack"],
       current_buy_in: game_state["current_buy_in"],
       current_bet: current_player["bet"],
-      raise: raise_amount
-    )
+      raise_amount: raise_amount
+    ).call
   end
 
   def showdown(game_state)
