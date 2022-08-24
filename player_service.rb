b9cadab025bbb3d87e1718c65cb775e791a4f1e6
Mateusz Sagan <mateusz@viaeurope.com>
2022-08-24 11:32:49 +0200

    Log parameters from requests

--- player_service.rb
+++ player_service.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'json'
 require_relative 'player'
+require "logger"
 
 set :port, 8090
 set :bind, '0.0.0.0'
 
 post "/" do
+  logger = Logger.new(STDOUT)
+  logger.error(JSON.parse(params))
   if params[:action] == 'bet_request'
     Player.new.bet_request(JSON.parse(params[:game_state])).to_s
   elsif params[:action] == 'showdown'
