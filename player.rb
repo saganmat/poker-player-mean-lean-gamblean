354ae3e07978004580687d98cad9708bc2e30b1a
Mateusz Sagan <mateusz@viaeurope.com>
2022-08-24 12:25:53 +0200

    Avoid raising more than the available funds

--- player.rb
+++ player.rb
@@ -12,12 +12,19 @@ class Player
 
     current_combination = current_hole_cards + community_cards
     hand_set = ::DetermineHandSet.new(current_combination).call
+    
+    raise_amount = if current_player["stack"] >= game_state["minimum_raise"]
+      game_state["minimum_raise"]
+    else
+      current_player["stack"]
+    end
 
     MakePlay.call(
       action: "call",
+      current_funds: current_player["stack"],
       current_buy_in: game_state["current_buy_in"],
       current_bet: current_player["bet"],
-      raise: game_state["minimum_raise"]
+      raise: raise_amount
     )
   end
 
